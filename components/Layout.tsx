19deff69582e3cd0d07bb5d04172e54c8ce10c8d
aidilrx04 <mrcrayv14@gmail.com>
2022-04-18 04:10:47 +0800

    add google auth id

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -11,6 +11,8 @@ function Layout({ children }) {
                 <meta charSet='utf-8' />
                 <meta name="description" content="Kuib web game" />
                 <meta name="keywords" content="kuib, quiz" />
+                <script src="https://apis.google.com/js/platform.js" async defer></script>
+                <meta name="google-signin-client_id" content="343047123354-lqeo2s4qk1dm57cgvhshb1lpc6c80ac0.apps.googleusercontent.com" />
             </Head>
             <Navigation />
             <main>
