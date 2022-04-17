60c6603f9b4fc8e087d1a9e2005732110a3d7f14
aidilrx04 <mrcrayv14@gmail.com>
2022-04-18 03:28:01 +0800

    changed firestore config to poo

--- util/db.js
+++ util/db.js
@@ -1,5 +1,13 @@
 // const fireConfig = require( "../config/db/firestore.json" );
-import fireConfig from '../config/db/firestore.json';
+// import fireConfig from '../config/db/firestore.json';
+const fireConfig = {
+    "apiKey": "AIzaSyA6vFOW36-uCIxc9POs0RHBSFhbH9rhsaM",
+    "authDomain": "kuib-6969.firebaseapp.com",
+    "projectId": "kuib-6969",
+    "storageBucket": "kuib-6969.appspot.com",
+    "messagingSenderId": "343047123354",
+    "appId": "1:343047123354:web:da18ad3528209d104a8a23"
+};
 
 import firebase from 'firebase/app';
 import 'firebase/auth';
