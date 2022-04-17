a7cac89fd10ed8af18caef62293e101dfae9ed3f
aidilrx04 <mrcrayv14@gmail.com>
2022-04-18 03:52:05 +0800

    add https protocol

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !== "production";
 
-const server = dev ? "http://localhost:3000" : "kuib.vercel.app";
+const server = dev ? "http://localhost:3000" : "https://kuib.vercel.app";
 
 export default server;
